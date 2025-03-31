0beda1d7e3182dd72b13f1dd0cba2741f4f532ac
VictorMerinoITCBCN <2023_victor.merino@iticbcn.cat>
2025-03-31 17:13:28 +0200

    improved book import script

--- libreria_paco/import/book/import.py
+++ libreria_paco/import/book/import.py
@@ -38,7 +38,6 @@ def import_book():
             description=row["description"],
             release=row["release"],
             cover=row["cover"],
-            author=Author.objects.get(id=row["author_id"]),
             oneStarCount=row.get("oneStarCount", 0),
             twoStarCount=row.get("twoStarCount", 0),
             threeStarCount=row.get("threeStarCount", 0),
@@ -47,6 +46,9 @@ def import_book():
         )
         book.save()
         
+        author_ids = row["author_ids"].split("|")
+        book.author.set(Author.objects.filter(id__in=author_ids))
+
         category_ids = row["category_ids"].split("|")
         book.category.set(Category.objects.filter(id__in=category_ids))
 
@@ -57,7 +59,7 @@ if __name__ == "__main__":
         "book": import_book
     }
     if (len(sys.argv) < 2 or sys.argv[1] not in validArgs.keys()): 
-        raise SyntaxError(f"No valid arguments.\nAvailable arguments: {validArgs}\nExample: python import/book/import.py author")
+        raise SyntaxError(f"No valid arguments.\nAvailable arguments: {validArgs.keys()}\nExample: python import/book/import.py author")
     
     key = sys.argv[1]
     validArgs[key]()
