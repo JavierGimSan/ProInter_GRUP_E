d06dfcc68b08fbcca9b66474cad614e2159d53d1
felix-balbin <2023_felix.balbin@iticbcn.cat>
2025-04-24 17:06:58 +0200

    urls

--- libreria_paco/libreria_paco/urls.py
+++ libreria_paco/libreria_paco/urls.py
@@ -19,8 +19,8 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('payment/', include('payment.urls')),
-    path('user/', include('user.urls'))
-    path('', include('book.urls'))
+    path('user/', include('user.urls')),
+    path('', include('book.urls')),
     path('payment/', include('payment.urls')),
     path('user/', include('user.urls')),
     path('', include('book.urls'))
