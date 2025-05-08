600f68ced49b8918be19d864ccce00e96657432d
VictorMerinoITCBCN <2023_victor.merino@iticbcn.cat>
2025-05-08 17:15:19 +0200

    added jwt auth

--- libreria_paco/user/models.py
+++ libreria_paco/user/models.py
@@ -1,26 +1,35 @@
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 from django.db import models
-from django.core.validators import MinLengthValidator, EmailValidator
-from django.core.exceptions import ValidationError
-from payment.models import Payment
-from role.models import Role
-# Create your models here.
+from django.core.validators import MinLengthValidator
 
-class User(models.Model):
-    name = models.TextField(validators=[MinLengthValidator(2)], max_length=100)
-    lastName = models.TextField(validators=[MinLengthValidator(2)], max_length=150)
-    email = models.TextField(validators=[MinLengthValidator(5)], max_length=320)
-    password = models.TextField(validators=[MinLengthValidator(2)])
-    roleId = models.ForeignKey(Role, on_delete=models.CASCADE)
-    paymentId = models.ForeignKey(Payment, on_delete=models.SET_NULL, null=True, blank=True)
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("El correo es obligatorio")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
 
-    def clean(self):
-        super().clean()
-        self.valid_email()
-        
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        return self.create_user(email, password, **extra_fields)
 
-    def valid_email(self):
-        validar = EmailValidator(message='Formato del correo electrónico incorrecto')
-        validar(self.email)
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=100, validators=[MinLengthValidator(2)])
+    lastName = models.CharField(max_length=150, validators=[MinLengthValidator(2)])
+    email = models.EmailField(unique=True)
+    roleId = models.ForeignKey('role.Role', on_delete=models.CASCADE)
+    paymentId = models.ForeignKey('payment.Payment', on_delete=models.SET_NULL, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-def __str__(self):
-    return self.email
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'lastName', 'roleId']
+
+    def __str__(self):
+        return self.email
--- libreria_paco/user/urls.py
+++ libreria_paco/user/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path, include
+from django.urls import path
 from user.views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
+    path('login/', TokenObtainPairView.as_view(), name='login'),
     path('registro-usuario/', crea_user, name='registro-usuario'),
     path('info-usuario/<str:pk>', info_user, name='informacion-usuario'),
     path('actualizar-usuario/<str:pk>',actualiza_user,name='actualizar-usuario'),
